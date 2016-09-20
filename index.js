3ce45be2d7b4e9c917ccedcd1ba6eb143fecdbb0
Andreas Hammer <andreas.hammer.97@protonmail.com>
2016-09-20 10:32:16 +0200

    Initial release

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function (dayOfWeek) {
+    dayOfWeek = Math.floor(dayOfWeek);
+    if(dayOfWeek < 0 || dayOfWeek > 6)
+        return null;
+    var date = new Date();
+    date.setHours(23);
+    date.setMinutes(59);
+    date.setSeconds(59);
+    var resultDate = new Date(date.getTime());
+
+    resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+
+    return resultDate;
+};
---
+++ test/test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var expect = require('chai').expect;
+var numFormatter = require('../index');
+
+describe('#numFormatter', function() {
+    it('should create sunday date', function() {
+        var result = numFormatter(0);
+        expect(result.getDay()).to.equal(0);
+    });
+
+    it('should create sunday date', function() {
+        var result = numFormatter(1);
+        expect(result.getDay()).to.equal(1);
+    });
+
+    it('should create sunday date', function() {
+        var result = numFormatter(2);
+        expect(result.getDay()).to.equal(2);
+    });
+
+    it('should create sunday date', function() {
+        var result = numFormatter(3);
+        expect(result.getDay()).to.equal(3);
+    });
+
+    it('should create sunday date', function() {
+        var result = numFormatter(4);
+        expect(result.getDay()).to.equal(4);
+    });
+
+    it('should create sunday date', function() {
+        var result = numFormatter(5);
+        expect(result.getDay()).to.equal(5);
+    });
+
+    it('should create sunday date', function() {
+        var result = numFormatter(6);
+        expect(result.getDay()).to.equal(6);
+    });
+
+    it('should return null', function(){
+        var result = numFormatter(-1);
+        expect(result).to.equal(null)
+    });
+
+    it('should return null', function(){
+        var result = numFormatter(7);
+        expect(result).to.equal(null);
+    });
+
+    it('should return sunday date', function(){
+        var result = numFormatter(0.9);
+        expect(result.getDay()).to.equal(0);
+    });
+});
